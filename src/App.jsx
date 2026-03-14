03051f8f4fef61def79bb6d10654b45a22135195
Himeth Silva <ghhrsilva@gmail.com>
2026-03-15 00:14:21 +0530

    portfolio website initial version

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <h1>Himeth Randil Silva</h1>
+      <p>Computer Science Undergraduate</p>
+
+      <h2>Projects</h2>
+      <ul>
+        <li>SDGP Project</li>
+        <li>IoT Smart Parking System</li>
+        <li>Frontend React Project</li>
+      </ul>
+
+      <h2>Links</h2>
+      <a href="https://github.com/ghhrsilva">GitHub</a>
+    </div>
+  )
+}
+
+export default App
